94a68e473b73dacf216cc087d35379188a841b99
bsj <875848407@qq.com>
2018-09-21 22:21:41 +0800

    app update

--- src/App.js
+++ src/App.js
@@ -4,33 +4,86 @@ import { Layout } from 'antd';
 import { connect } from 'react-redux';
 import { actionCreators as commonActionCreators } from './store/common';
 import { actionCreators as sidebarActionCreators } from '@/components/SideBar/store';
+import { ContainerQuery } from 'react-container-query';
+import { enquireScreen, unenquireScreen } from 'enquire-js';
+import DocumentTitle from 'react-document-title';
 import SideBar from './components/SideBar';
 import HeadBar from './components/HeadBar';
+import classNames from 'classnames';
 import './App.less';
 import styles from './App.less';
 
 const { Sider, Content } = Layout;
 
+const query = {
+  'screen-xs': {
+    maxWidth: 575
+  },
+  'screen-sm': {
+    minWidth: 576,
+    maxWidth: 767
+  },
+  'screen-md': {
+    minWidth: 768,
+    maxWidth: 991
+  },
+  'screen-lg': {
+    minWidth: 992,
+    maxWidth: 1199
+  },
+  'screen-xl': {
+    minWidth: 1200,
+    maxWidth: 1599
+  },
+  'screen-xxl': {
+    minWidth: 1600
+  }
+};
+
 class App extends Component {
+  state = {
+    isMobile: false
+  };
+
   componentDidMount() {
     const { menu_update } = this.props;
     menu_update();
+    this.enquireHandler = enquireScreen(mobile => {
+      const { isMobile } = this.state;
+      if (isMobile !== mobile) {
+        this.setState({
+          isMobile: mobile
+        });
+      }
+    });
   }
+
+  componentWillUnmount() {
+    unenquireScreen(this.enquireHandler);
+  }
+
+  getPageTitle = pathname => {
+    return 'pathname';
+  };
 
   render() {
     const { collapsed } = this.props;
+    const { isMobile } = this.state;
 
-    return (
+    const layout = (
       <Layout className={styles.App}>
-        <Sider
-          className={styles.sider}
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-        >
-          <div className={styles.logo} />
-          <SideBar />
-        </Sider>
+        {!isMobile && (
+          <Sider
+            className={styles.sider}
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+          >
+            <div className={styles.logo} />
+            <SideBar />
+          </Sider>
+        )}
+
         <Layout>
           <HeadBar />
           <Content
@@ -45,6 +98,14 @@ class App extends Component {
           </Content>
         </Layout>
       </Layout>
+    );
+
+    return (
+      <DocumentTitle title={this.getPageTitle('pathname')}>
+        <ContainerQuery query={query}>
+          {params => <div className={classNames(params)}>{layout}</div>}
+        </ContainerQuery>
+      </DocumentTitle>
     );
   }
 }
